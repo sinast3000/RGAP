6343fc0de84168f8a631f98c4f4b4d5c440e789c
sinast3000 <streicher@kof.ethz.ch>
2022-04-05 17:39:39 +0200

    simplified MLE prediction and clean up

--- R/prediction.R
+++ R/prediction.R
@@ -14,6 +14,9 @@
 #'   list is returned.
 #' @inheritParams trendAnchor
 #' 
+#' @return The fitted object with an updated time series list \code{tsl}. If 
+#'   \code{returnFit = FALSE}, only the updated time series list is returned.
+#' 
 #' @export
 predict <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE) {
 
@@ -35,8 +38,6 @@ predict <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE) {
 #'
 #' @inheritParams predict
 #' 
-#' @details Method only applicable for models estimated via MLE.
-#' 
 #' @keywords internal
 predictMLE <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE) {
 
@@ -46,168 +47,34 @@ predictMLE <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE) 
   if (dim(model$Z)[3] > 1) {
     Z <- model$Z
     if (exogenous == "last") {
-      Z <- Z[, , n]
+      Z <- Z[, , dim(Z)[3]]
     } else if (exogenous == "mean") {
       Z <- apply(Z, 1:2, mean)
     }
-    # model$Z <- array(c(c(model$Z), rep(model$Z[, , dim(model$Z)[3]], n.ahead)), dim(model$Z) + c(0, 0, n.ahead))
     model$Z <- array(c(c(model$Z), rep(Z, n.ahead)), dim(model$Z) + c(0, 0, n.ahead))
   }
-  attr(model, "n") <-   attr(model, "n") + as.integer(n.ahead)
+  n_fc <- attr(model, "n") + as.integer(n.ahead)
+  attr(model, "n") <- n_fc
   
   # filter and smoother
   out_fc <- KFS(model, simplify = FALSE, filtering = c("state", "signal"), smoothing = c("state", "signal", "disturbance"))
   
-  # get relevant time series
-  V <- out_fc$V
-  alphahat <- out_fc$alphahat
-  att <- out_fc$att
-  Ptt <- out_fc$Ptt
+  # get observation equation and add forecast
   y <- fit$model$SSModel$y
   y <- window(y, end = end(y) + c(0, n.ahead), extend = TRUE)
+  y[is.na(y)] <- out_fc$m[is.na(y)] # fill NAs with predictions
+  out_fc$model$y <- y
   
-  # state equation parameter matrices
-  n <- attr(model, "n")
-  TT <- model$T[, , 1]
-  R <- model$R[, , 1]
-  Q <- model$Q[, , 1]
-  Z <- model$Z
-  H <- model$H[, , 1]
+  # get results
+  tsl_fc <- .SSresults(out = out_fc, model = fit$model, prediction = TRUE)
   
-  # # fit$model$SSModel$y <- window(fit$model$SSModel$y, 
-  # #                               end = end(fit$model$SSModel$y) + c(0, n.ahead), extend = TRUE)
-  # # Z_new <- array(NA, dim(Z) + c(0,0,n.ahead))
-  # # Z_new[, , 1:dim(Z)[3]] <- Z
-  # # Z_new[, , -c(1:dim(Z)[3])] <- Z[, , n]
-  # # fit$model$SSModel$Z <- Z_new
-  # # out <- KFS(fit$model$SSModel, simplify = FALSE, filtering = c("state", "signal"), smoothing = c("state", "signal", "disturbance"))
-  # # 
-  # if (exogenous == "last") {
-  #   Z <- Z[, , n]
-  # } else if (exogenous == "mean") {
-  #   Z <- apply(Z, 1:2, mean)
-  # }
-  # 
-  # # smoothed state and variance
-  # alphahat <- fit$SSMout$alphahat
-  # V <- fit$SSMout$V
-  # alphahat <- window(alphahat, end = end(alphahat) + c(0, n.ahead), extend = TRUE)
-  # V <- array(c(V, rep(NA, NROW(V) *  NCOL(V) * n.ahead)), c(NROW(V), NCOL(V),n + n.ahead))
-  # 
-  # # filtered state and variance
-  # att <- fit$SSMout$att
-  # Ptt <- fit$SSMout$Ptt
-  # att <- window(att, end = end(alphahat) + c(0, n.ahead), extend = TRUE)
-  # Ptt <- array(c(Ptt, rep(NA, NROW(V) *  NCOL(Ptt) * n.ahead)), c(NROW(Ptt), NCOL(Ptt),n + n.ahead))
+  # add standard error for forecast of observation equation
+  V_mu <- out_fc$V_mu
+  tsl_fc$obsSE <- ts(rbind(
+      matrix(0, n_fc - n.ahead, NCOL(y)),
+      t(sqrt(apply(out_fc$V_mu[, , (n_fc - n.ahead + 1):n_fc], 3, diag)))
+    ), start = start(y), frequency = frequency(y))
 
-  # # observables
-  # y <- fit$model$SSModel$y
-  # y <- window(y, end = end(y) + c(0, n.ahead), extend = TRUE)
-
-  # # helper matrices
-  # RQR <- R %*% Q %*% t(R)
-  # 
-  # # predict
-  # for (tt in 1:n.ahead) {
-  #   # smoothed and filtered state
-  #   alphahat[n + tt, ] <- TT %*% alphahat[n + tt - 1, ]
-  #   att[n + tt, ] <- TT %*% att[n + tt - 1, ]
-  #   
-  #   # variances of smoothed and filtered state
-  #   # V[, , n + tt] <- Ptt[, , n + tt] <- RQRtt + (TT^tt) %*% V[, , n] %*% t(TT^tt)
-  #   V[, , n + tt] <- Ptt[, , n + tt] <- RQR + TT %*% V[, , n + tt - 1] %*% t(TT)
-  #   # V[, , n] equals Ptt[, , n]
-  #   
-  # }
-
-  
-  # predict observations
-  # y_fitted <- ts(alphahat %*% t(Z), start = start(alphahat), frequency = frequency(alphahat))
-  if (dim(model$Z)[3] > 1) {
-    y_fitted <- ts(t(sapply(1:dim(Z)[3], function(x) alphahat[x, ] %*% t(Z[, , x]))), 
-              start = start(alphahat), frequency = frequency(alphahat))
-    V_y <- array(sapply(1:dim(V)[3], function(x) Z[, , x] %*% V[, , x] %*% t(Z[, , x]) + H), c(rep(dim(Z)[1], 2), dim(V)[3]))
-  } else {
-    y_fitted <- ts(alphahat %*% t(Z[, , 1]), start = start(alphahat), frequency = frequency(alphahat))
-    V_y <- array(sapply(1:dim(V)[3], function(x) Z[, , 1] %*% V[, , x] %*% t(Z[, , 1]) + H), c(rep(dim(Z)[1], 2), dim(V)[3]))
-  }
-  V_y <- array(sapply(1:dim(V)[3], function(x) { # set values for which observations exist to NA
-    mat <- V_y[, , x]
-    diag(mat)[!is.na(y)[x,]] <- NA
-    mat
-  }), dim(V_y))
-  y[is.na(y)] <- y_fitted[is.na(y)] # fill NAs with predictions
-  
-  # confidence intervals smoothed state
-  alphahat_se <- ts(t(sqrt(apply(V, 3, diag))), start = start(alphahat), frequency = frequency(alphahat))
-  colnames(alphahat_se) <- colnames(alphahat)
-
-  # confidence intervals filtered state
-  att_se <- ts(t(sqrt(apply(Ptt, 3, diag))), start = start(att), frequency = frequency(att))
-  colnames(att_se) <- colnames(att)
-  
-  # confidence interval obervation equation
-  y_se <- ts(t(sqrt(apply(V_y, 3, diag))), start = start(alphahat), frequency = frequency(alphahat))
-  colnames(y_se) <- colnames(y)
-
-  # new ts list
-  tsl_fc <- within(fit$tsl, {
-    obs <- y
-    obsSE <- y_se
-    stateSmoothed <- alphahat
-    stateFiltered <- att
-    stateSmoothedSE <- alphahat_se
-    stateFilteredSE <- att_se
-  })
-  
-  # fill ts list depending on different objects
-  trend <- alphahat[, "trend"]
-  trend_se <- alphahat_se[, "trend"]
-  cycle <- alphahat[, "cycle"]
-  cycle_se <- alphahat_se[, "cycle"]
-  
-  # out_fc <- fit$SSMout
-  # out_fc$alphahat <- alphahat
-  # out_fc$V <- V
-  out_fc$V_y <- V_y
-  # out_fc$dims$n <- fit$SSMout$dims$n + n.ahead
-  if (inherits(fit, "NAWRUfit")) {
-    
-    tsl_fc <- within(tsl_fc, {
-      nawru <- trend
-      nawruSE <- trend_se
-    })
-    
-  } else if (inherits(fit, "TFPfit")) {
-    
-    tsl_fc <- within(tsl_fc, {
-      tfpTrend <- exp(trend)
-      tfpTrendGrowth <- growth(exp(trend))
-      tfp <- exp(y[, 1])
-      tfpGrowth <- growth(exp(y[, 1]))
-    })
-    
-    tsl_fc[c("tfpTrendSE", "tfpTrendGrowthSE")] <- .deltaMethodState(out = out_fc, nameState = "trend")[-1]
-    tsl_fc$tfpSE <- .deltaMethodObs(out = out_fc, y = y, nameObs = "logtfp")$expObsSE
-    tsl_fc$tfpGrowthSE <- .deltaMethodObs(out = out_fc, y = y, nameObs = "logtfp")$diffObsSE
-    
-  } else if (inherits(fit, "KuttnerFit")) {
-    
-    tsl_fc <- within(tsl_fc, {
-      potential <- exp(trend)
-      potentialGrowth <- growth(exp(trend))
-      gap <- cycle * 100
-      gdp <- exp(y[, 1])
-      gdpGrowth <- growth(exp(y[, 1]))
-    })
-    
-    tsl_fc[[c("potentialGrowthSE")]] <- .deltaMethodState(out = out_fc, nameState = "trend")$diffStateSE
-    tsl_fc[[c("potentialSE")]] <- .deltaMethodState(out = out_fc, nameState = "trend")$expStateSE
-    tsl_fc[[c("gapSE")]] <- 100 * .deltaMethodState(out = out_fc, nameState = "cycle")$StateSE
-    tsl_fc$gdpSE <- .deltaMethodObs(out = out_fc, y = y, nameObs = "loggdp")$expObsSE
-    tsl_fc$gdpGrowthSE <- .deltaMethodObs(out = out_fc, y = y, nameObs = "loggdp")$diffObsSE
-  }
-  
   # return
   if (returnFit) {
     fit$tsl <- tsl_fc
@@ -226,12 +93,10 @@ predictMLE <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE) 
 
 #' Predictions for Bayesian estimation
 #'
-#' @description Computes predictions for an object of class \code{fitNAWRU, fitTFP, fitKuttner}
-#' estimated via MLE.
+#' @description Computes predictions for an object of class \code{fitNAWRU, fitTFP}
+#' estimated via Bayesian methods.
 #'
 #' @inheritParams predict
-#' 
-#' @details Method only applicable for models estimated via MLE.
 #' 
 #' @keywords internal
 predictBayes <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE) {
@@ -279,8 +144,6 @@ predictBayes <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE
     )
     n <- attr(fit$SSMfit$model, "n")
     TT <- SSModel$T[, , 1]
-    # R <- SSModel$R[, , 1]
-    # Q <- SSModel$Q[, , 1]
     Z <- SSModel$Z
     if (exogenous == "last") {
       Z <- Z[, , n]
@@ -302,7 +165,6 @@ predictBayes <- function(fit, n.ahead = 10, exogenous = "mean", returnFit = TRUE
       # smoothed and filtered state
       alphahat[n + tt, ] <- att[n + tt, ] <- TT %*% alphahat[n + tt - 1, ]
       # alphahat[n, ] equals att[n, ]
-
     }
     state_fc[, , k] <- alphahat
     state_f_fc[, , k] <- att
